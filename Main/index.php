bffffd9bcb11495d58309d8545ff3ea22df9418c
Seweryn04 <113586665+Seweryn04@users.noreply.github.com>
2024-04-10 11:12:58 +0200

    Add files via upload

---
+++ Main/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Aquí empieza el proyecto...</title>
+		<link rel="icon" type="image/x-icon" href="img/logo16.png">
+        <link rel="stylesheet" href="indexCSS.css">
+	</head>
+	<body>
+		<header>
+            <div id="logo">
+                <img src="./img/logo128.png" alt="logo">
+            </div>
+            <hr>
+            <div id="Menu">
+                <ul>
+                    <li><a href="">Ayuda</a></li>
+                    <li><a href="">Info</a></li>
+                    <li><a href="">Cuenta</a></li>
+                    <li><a href="">Cesta</a></li>
+                </ul>
+            </div>
+            <hr>
+        </header>
+	</body>
+</html>
